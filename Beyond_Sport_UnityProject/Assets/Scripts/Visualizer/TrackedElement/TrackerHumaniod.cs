d90e863480fc51a68e2c5d4219732d573fa91645
Jesse Stam <nolimet@stamdata.nl>
2021-10-12 21:13:47 +0200

    added a TrackedHumanoid

---
+++ Beyond_Sport_UnityProject/Assets/Scripts/Visualizer/TrackedElement/TrackerHumaniod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace BeyondSports.Visualizer
+{
+    public class TrackedHumanoid : BaseTrackedObject
+    {
+        public class Factory : PlaceholderFactory<TrackedHumanoid>
+        {
+            public TrackedHumanoid Create(Color color)
+            {
+                var newTrackedHumanoid = base.Create();
+                newTrackedHumanoid.SetColor(color);
+
+                return newTrackedHumanoid;
+            }
+        }
+    }
+}
