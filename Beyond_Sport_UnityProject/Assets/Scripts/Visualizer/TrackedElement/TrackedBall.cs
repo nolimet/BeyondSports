e5f6308d7e315db60e0711c1dfaf54876cb9de76
Jesse Stam <nolimet@stamdata.nl>
2021-10-13 00:27:16 +0200

    Added Tracked ball and Humanoid

---
+++ Beyond_Sport_UnityProject/Assets/Scripts/Visualizer/TrackedElement/TrackedBall.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace BeyondSports.Visualizer
+{
+    public class TrackedBall : BaseTrackedObject
+    {
+        public class Factory : PlaceholderFactory<TrackedBall>
+        {
+            public TrackedBall Create(Color color)
+            {
+                var newTrackedBall = base.Create();
+                newTrackedBall.SetColor(color);
+
+                return newTrackedBall;
+            }
+        }
+    }
+}
