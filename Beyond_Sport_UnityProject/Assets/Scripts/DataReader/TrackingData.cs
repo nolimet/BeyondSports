7a7dfc3284bdb492944158574d452378e7da5d92
Jesse Stam <nolimet@stamdata.nl>
2021-10-12 17:27:28 +0200

    Added a first version of TrackingData

---
+++ Beyond_Sport_UnityProject/Assets/Scripts/DataReader/TrackingData.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeyondSports.DataReader
+{
+    public class TrackingData
+    {
+        public readonly TrackingFrame[] frames;
+
+        public TrackingData(TrackingFrame[] frames)
+        {
+            this.frames = frames;
+        }
+    }
+
+    public readonly struct TrackingFrame
+    {
+        public readonly long frameID;
+        public readonly TrackedObject[] trackedObjects;
+        public readonly BallData ball;
+
+        public TrackingFrame(long frameID, TrackedObject[] trackedObjects, BallData ball)
+        {
+            this.frameID = frameID;
+            this.trackedObjects = trackedObjects;
+            this.ball = ball;
+        }
+    }
+
+    public readonly struct BallData
+    {
+        public readonly Vector3 position;
+        public readonly double speed;
+        public readonly string flag;
+
+        public BallData(Vector3 position, double speed, string flag)
+        {
+            this.position = position;
+            this.speed = speed;
+            this.flag = flag;
+        }
+    }
+
+    public readonly struct TrackedObject
+    {
+        public readonly int id;
+        public readonly int teamId;
+        public readonly int shirtNumber;
+        public readonly Vector3 position;
+        public readonly double speed;
+
+        public TrackedObject(int id, int teamId, int shirtNumber, Vector3 position, double speed)
+        {
+            this.id = id;
+            this.teamId = teamId;
+            this.shirtNumber = shirtNumber;
+            this.position = position;
+            this.speed = speed;
+        }
+    }
+}
