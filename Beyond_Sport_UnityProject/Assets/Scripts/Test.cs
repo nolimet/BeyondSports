e85cedcb28cdc43f56273551cfee5be4d739aa1a
Jesse Stam <nolimet@stamdata.nl>
2021-10-12 17:30:49 +0200

    added test script

---
+++ Beyond_Sport_UnityProject/Assets/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using BeyondSports.DataReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Test : MonoBehaviour
+    {
+        private TrackingDataReaderService dataReaderService = new TrackingDataReaderService(new StaticSoccerDataReader());
+
+        [SerializeField]
+        private string fileName = "match_data.dat";
+
+        public async void Start()
+        {
+            await dataReaderService.SetReaderPath($"File://{Application.streamingAssetsPath}/{fileName}");
+
+            Debug.Log(await dataReaderService.GetFrame(0));
+        }
+    }
+}
