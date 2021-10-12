7e8ea59a265ecd2ace54e25f7bc7d7eb33817c1e
Jesse Stam <nolimet@stamdata.nl>
2021-10-12 21:13:24 +0200

    Added baseTrackedObject

---
+++ Beyond_Sport_UnityProject/Assets/Scripts/Visualizer/TrackedElement/BaseTrackedObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BeyondSports.Visualizer
+{
+    public abstract class BaseTrackedObject : MonoBehaviour
+    {
+        [SerializeField]
+        private Renderer[] renderers;
+
+        public int id { get; private set; }
+
+        protected void SetColor(Color color)
+        {
+            foreach (var renderer in renderers)
+            {
+                renderer.material.color = color;
+            }
+        }
+
+        public void SetPosition(Vector3 position)
+        {
+            transform.position = position;
+        }
+    }
+}
