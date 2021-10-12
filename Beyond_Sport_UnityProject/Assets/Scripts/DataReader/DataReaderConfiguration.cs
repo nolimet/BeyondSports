6ac2a0c7b14fe77b834f7cae0464ab42ecf83dec
Jesse Stam <nolimet@stamdata.nl>
2021-10-13 00:24:25 +0200

    Added Data Reader configuration

---
+++ Beyond_Sport_UnityProject/Assets/Scripts/DataReader/DataReaderConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BeyondSports.DataReader
+{
+    [CreateAssetMenu(fileName = "Data Reader Configuration", menuName = "Configuration/Data Reader")]
+    public class DataReaderConfiguration : ScriptableObject
+    {
+        [SerializeField]
+        private ReaderTypes readerType;
+
+        [SerializeField]
+        private PathMode pathMode;
+
+        [SerializeField]
+        private string filePath;
+
+        public string FilePath => filePath;
+        public ReaderTypes ReaderType => readerType;
+        public PathMode PathMode => pathMode;
+    }
+}
---
+++ Beyond_Sport_UnityProject/Assets/Scripts/DataReader/PathMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyondSports.DataReader
+{
+    public enum PathMode
+    {
+        Absolute,
+        Relative_StreamingAssets,
+        Relative_DataPath,
+    }
+}
