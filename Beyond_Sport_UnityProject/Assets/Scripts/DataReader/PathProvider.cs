65353fbd77ce573d9636015d94d3e235b8d846b6
Jesse Stam <nolimet@stamdata.nl>
2021-10-13 00:25:16 +0200

    Created a path provider
    Will be used to generate relative paths in on location

---
+++ Beyond_Sport_UnityProject/Assets/Scripts/DataReader/PathProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BeyondSports.DataReader
+{
+    public static class PathProvider
+    {
+        public static string GetPathFromConfiguration(DataReaderConfiguration configuration)
+        {
+            return GetPath(configuration.PathMode, configuration.FilePath);
+        }
+
+        public static string GetPath(PathMode pathMode, string filePath)
+        {
+            //TODO add device specifc path
+            switch (pathMode)
+            {
+                case PathMode.Absolute:
+                    return filePath;
+
+                case PathMode.Relative_StreamingAssets:
+                    return $"File://{Application.streamingAssetsPath}/{filePath}";
+
+                case PathMode.Relative_DataPath:
+                    return $"File://{Application.dataPath}/{filePath}";
+            }
+
+            return string.Empty;
+        }
+    }
+}
