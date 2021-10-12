44b1a46cea1f67a2fd3355109ab9a986b3d59416
Jesse Stam <nolimet@stamdata.nl>
2021-10-12 17:28:08 +0200

    added reader service

---
+++ Beyond_Sport_UnityProject/Assets/Scripts/DataReader/TrackingDataReaderService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyondSports.DataReader
+{
+    public class TrackingDataReaderService
+    {
+        public TrackingDataReaderService(IDataReader dataReader)
+        {
+            this.dataReader = dataReader;
+        }
+
+        private readonly IDataReader dataReader;
+
+        public async Task<TrackingFrame> GetFrame(long frameID)
+        {
+            return await dataReader.GetFrame(frameID);
+        }
+
+        public async Task<long> GetFrameCount()
+        {
+            return await dataReader.GetFrameCount();
+        }
+
+        public async Task SetReaderPath(string path)
+        {
+            await dataReader.SetDataPath(path);
+        }
+    }
+}
