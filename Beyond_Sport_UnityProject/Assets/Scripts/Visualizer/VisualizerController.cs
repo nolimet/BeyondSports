31dfd5d52feba2672680ca5d302dac6dff8345e4
Jesse Stam <nolimet@stamdata.nl>
2021-10-13 14:14:55 +0200

    Implemented first part of visualizerController

--- Beyond_Sport_UnityProject/Assets/Scripts/Visualizer/VisualizerController.cs
+++ Beyond_Sport_UnityProject/Assets/Scripts/Visualizer/VisualizerController.cs
@@ -1,6 +1,36 @@
-﻿namespace BeyondSports.Visualizer
+﻿using BeyondSports.DataReader;
+using System.Collections.Generic;
+
+namespace BeyondSports.Visualizer
 {
     public class VisualizerController
     {
+        private readonly TrackedObject.Factory objectFactory;
+        private readonly TrackedBall.Factory ballFactory;
+
+        private readonly List<TrackedObject> trackedObjects = new List<TrackedObject>();
+        private readonly List<TrackedBall> trackedBalls = new List<TrackedBall>();
+
+        public VisualizerController(TrackedObject.Factory objectFactory, TrackedBall.Factory ballFactory)
+        {
+            this.objectFactory = objectFactory;
+            this.ballFactory = ballFactory;
+        }
+
+        public void ApplyFrame(TrackingFrame frame)
+        {
+            if (frame.trackedObjects.Length > trackedObjects.Count)
+            {
+                int missingObjects = frame.trackedObjects.Length - trackedObjects.Count;
+                for (int i = 0; i < missingObjects; i++)
+                {
+                    trackedObjects.Add(objectFactory.Create());
+                }
+            }
+
+            if (frame.balls.Length > trackedBalls.Count)
+            {
+            }
+        }
     }
 }
