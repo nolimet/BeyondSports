886940382a8a2e048f14e7919a7cb7bec7b8155c
Jesse Stam <nolimet@stamdata.nl>
2021-10-13 21:07:53 +0200

    added PlayBackUIController

---
+++ Beyond_Sport_UnityProject/Assets/Scripts/Playerback/PlaybackUIController.cs
@@ -0,0 +1,30 @@
+using BeyondSports.Playerback;
+using UnityEngine;
+using Zenject;
+
+public class PlaybackUIController : MonoBehaviour
+{
+    private PlaybackController playbackController;
+    private (long start, long end) frameRange;
+
+    [Inject]
+    public void Inject(PlaybackController playbackController)
+    {
+        this.playbackController = playbackController;
+    }
+
+    public async void LoadFirstFrame()
+    {
+        frameRange = await playbackController.LoadFirstFrame();
+    }
+
+    public async void SetFirstFrame()
+    {
+        await playbackController.JumpToFrame(frameRange.start);
+    }
+
+    public async void JumpToFrame(int value)
+    {
+        await playbackController.JumpToFrame(value);
+    }
+}
