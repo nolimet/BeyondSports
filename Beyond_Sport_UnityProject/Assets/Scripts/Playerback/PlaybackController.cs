358c0a4531b35bedf4745b6c6339c15b5b8dffc8
Jesse Stam <nolimet@stamdata.nl>
2021-10-13 19:54:47 +0200

    Added get first frame function to playback controller

--- Beyond_Sport_UnityProject/Assets/Scripts/Playerback/PlaybackController.cs
+++ Beyond_Sport_UnityProject/Assets/Scripts/Playerback/PlaybackController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BeyondSports.DataReader;
+using BeyondSports.Visualizer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +13,25 @@ namespace BeyondSports.Playerback
         private readonly DataReader.TrackingDataReaderService readerService;
         private readonly DataReader.DataReaderConfiguration readerConfiguration;
         private readonly Visualizer.VisualizerController visualizerController;
+
+        public PlaybackController(TrackingDataReaderService readerService, DataReaderConfiguration readerConfiguration, VisualizerController visualizerController)
+        {
+            this.readerService = readerService;
+            this.readerConfiguration = readerConfiguration;
+            this.visualizerController = visualizerController;
+        }
+
+        public async Task<(long start, long end)> LoadFirstFrame()
+        {
+            await readerService.SetReaderPath(readerConfiguration.GetPath());
+            var frameRange = await readerService.GetFrameRange();
+
+            visualizerController.ApplyFrame(await readerService.GetFrame(frameRange.start));
+            return frameRange;
+        }
+
+        public void JumpToFrame(int value)
+        {
+        }
     }
 }
